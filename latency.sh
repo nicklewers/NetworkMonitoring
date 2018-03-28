088cf0280af686843b653e342dd91813b5018396
nicklewers <nicklewers@Nicks-MacBook-Pro-3.local>
2018-03-28 18:35:02 +0100

    attempt #2 fix: change print to echo

--- latency.sh
+++ latency.sh
@@ -3,16 +3,16 @@
     # latency data to be gained: minimums, averages and maximums for round trip
 
   echo $(date)
-  print "\n www.potsandpithoi.com"
+  echo "\n www.potsandpithoi.com"
   ping -c 30 www.potsandpithoi.com | tail -2
 
-  print "\n www.babines-bakery.com"
+  echo "\n www.babines-bakery.com"
   ping -c 30 www.babines-bakery.com | tail -2
 
 
-  print "\n www.waterfallgardens.com"
+  echo "\n www.waterfallgardens.com"
   ping -c 30 www.waterfallgardens.com | tail -2
 
-  print "\n www.udayanaecolodge.com"
+  echo "\n www.udayanaecolodge.com"
   ping -c 30 www.udayanaecolodge.com | tail -2
-  print "= = = = = = = = = = = = = = = = = \n"
+  echo "= = = = = = = = = = = = = = = = = \n"
