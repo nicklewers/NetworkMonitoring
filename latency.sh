2852366b5f5abf7e8e09caa33696cf556744b14c
nicklewers <nicklewers@Nicks-MacBook-Pro-3.local>
2018-03-28 18:22:59 +0100

    added support for displaying packet loss to log

--- latency.sh
+++ latency.sh
@@ -5,15 +5,15 @@
 
   print $(date)
   print "\n www.potsandpithoi.com"
-  ping -c 30 www.potsandpithoi.com | tail -1| awk '{print $4}'
+  ping -c 30 www.potsandpithoi.com | tail -2
 
   print "\n www.babines-bakery.com"
-  ping -c 30 www.babines-bakery.com | tail -1| awk '{print $4}'
+  ping -c 30 www.babines-bakery.com | tail -2
 
 
   print "\n www.waterfallgardens.com"
-  ping -c 30 www.waterfallgardens.com | tail -1| awk '{print $4}'
+  ping -c 30 www.waterfallgardens.com | tail -2
 
   print "\n www.udayanaecolodge.com"
-  ping -c 30 www.udayanaecolodge.com | tail -1| awk '{print $4}'
+  ping -c 30 www.udayanaecolodge.com | tail -2
   print "= = = = = = = = = = = = = = = = = \n"
