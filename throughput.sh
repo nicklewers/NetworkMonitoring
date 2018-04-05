250d54f5a1fdcebe41f16c3c8c0739a559a55b30
nicklewers <nicklewers@eduroam-16-185.uea.ac.uk>
2018-04-05 15:46:34 +0100

    changed mirrors for wget

--- throughput.sh
+++ throughput.sh
@@ -1,10 +1,10 @@
 # download 3 different files sizes 10 times each on a specific time/day
 
 # small file
-wget -O ~/net_monitor/downloads/100kb.zip http://speedtest.tele2.net/100KB.zip
+wget -O ~/net_monitor/downloads/100kb.txt https://mirror.aarnet.edu.au/pub/ftp.kernel.org/linux/docs/lanana/device-list/devices-2.6.txt
 
 # medium file
-wget -O ~/net_monitor/downloads/2mb.zip http://speedtest.tele2.net/2MB.zip
+wget -O ~/net_monitor/downloads/2mb.zip https://mirror.aarnet.edu.au/pub/ftp.kernel.org/linux/docs/man-pages/man-pages-3.80.tar.gz
 
 # large file
-wget -O ~/net_monitor/downloads/100mb.zip http://speedtest.tele2.net/100MB.zip
+wget -O ~/net_monitor/downloads/100mb.zip https://mirror.aarnet.edu.au/pub/TED-talks/RobinChase_2012G-480p.mp4
