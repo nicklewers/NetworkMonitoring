1e96098424bada68461ca134b4f6cf0704860b82
Nick Lewers <nicklewers@gmail.com>
2018-04-05 13:59:07 +0100

    updated scripts  to use printf rather than echo in parsing, to enable better formatting

--- latency.sh
+++ latency.sh
@@ -2,17 +2,17 @@
     # send 10 packets per host (enough to get conclusive packet loss results)
     # latency data to be gained: minimums, averages and maximums for round trip
 
-  echo $(date)
-  echo "\n www.linkwan.com"
+  printf "$(date)"
+  printf "\n www.linkwan.com"
   ping -c 10 www.linkwan.com | tail -2
 
-  echo "\n www.jaguar-network.com"
+  printf "\n www.jaguar-network.com"
   ping -c 10 www.jaguar-network.com | tail -2
 
 
-  echo "\n www.rhnet.is"
+  printf "\n www.rhnet.is"
   ping -c 10 www.rhnet.is | tail -2
 
-  echo "\n www.zintana.com.br"
+  printf "\n www.zintana.com.br"
   ping -c 10 www.zintana.com.br | tail -2
-  echo "= = = = = = = = = = = = = = = = = \n"
+  printf  "= = = = = = = = = = = = = = = = = \n"
--- traceroute.sh
+++ traceroute.sh
@@ -1,17 +1,17 @@
 # perform traceroute on each host
   # ive found hosts which allow a full traceroute with no firewall blocks (*)
 
-  echo $(date)
-  echo "\n www.linkwan.com"
+  printf " $(date)"
+  printf "\n www.linkwan.com"
   traceroute www.linkwan.com
 
-  echo "\n www.jaguar-network.com"
+  printf "\n www.jaguar-network.com"
   traceroute www.jaguar-network.com
 
-  echo "\n www.rhnet.com"
+  printf "\n www.rhnet.com"
   traceroute www.rhnet.com
 
-  echo "\n www.zintana.com.br"
+  printf "\n www.zintana.com.br"
   traceroute www.zintana.com.br
 
-  echo "= = = = = = = = = = = = = = = = = \n"
+  printf "= = = = = = = = = = = = = = = = = \n"
