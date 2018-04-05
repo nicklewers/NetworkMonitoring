fb4578632d61f4f7cac36d17495bc642e99a49b7
nicklewers <nicklewers@eduroam-16-185.uea.ac.uk>
2018-04-05 15:35:12 +0100

    wget for 3 file sizes

--- throughput.sh
+++ throughput.sh
@@ -1,1 +1,10 @@
 # download 3 different files sizes 10 times each on a specific time/day
+
+# small file
+wget -O ~/net_monitor/downloads/100kb.zip http://speedtest.tele2.net/100KB.zip
+
+# medium file
+wget -O ~/net_monitor/downloads/2mb.zip http://speedtest.tele2.net/2MB.zip
+
+# large file
+wget -O ~/net_monitor/downloads/100mb.zip http://speedtest.tele2.net/100MB.zip
