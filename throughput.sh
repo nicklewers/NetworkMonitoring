9f48af23ab3299370534f833f784ce7364a6fb06
nicklewers <nicklewers@eduroam-19-240.uea.ac.uk>
2018-04-06 13:58:22 +0100

    modifed to output throughput directly into file

--- throughput.sh
+++ throughput.sh
@@ -1,18 +1,18 @@
 # download 3 different files sizes 10 times each on a specific time/day
-printf "\n ====== BEGIN LOOP ====== "
+printf "\n ====== BEGIN LOOP ====== " >> ~/net_monitor/throughput.log
 # small file
-printf "\n small  file"
-wget -O ~/net_monitor/downloads/100kb.txt https://mirror.aarnet.edu.au/pub/ftp.kernel.org/linux/docs/lanana/device-list/devices-2.6.txt
+printf "\n small  file" >> ~/net_monitor/throughput.log
+wget -O ~/net_monitor/downloads/100kb.txt https://mirror.aarnet.edu.au/pub/ftp.kernel.org/linux/docs/lanana/device-list/devices-2.6.txt 1 >> ~/net_monitor/throughput.log
 
-printf "\n medium file"
+printf "\n medium file" >> ~/net_monitor/throughput.log
 # medium file
-wget -O ~/net_monitor/downloads/2mb.zip https://mirror.aarnet.edu.au/pub/ftp.kernel.org/linux/docs/man-pages/man-pages-3.80.tar.gz
+wget -O ~/net_monitor/downloads/2mb.zip https://mirror.aarnet.edu.au/pub/ftp.kernel.org/linux/docs/man-pages/man-pages-3.80.tar.gz >> ~/net_monitor/throughput.log
 
-printf "\n large file"
+printf "\n large file" >> ~/net_monitor/throughput.log
 # large file
-wget -O ~/net_monitor/downloads/100mb.zip https://mirror.aarnet.edu.au/pub/TED-talks/RobinChase_2012G-480p.mp4
+wget -O ~/net_monitor/downloads/100mb.zip https://mirror.aarnet.edu.au/pub/TED-talks/RobinChase_2012G-480p.mp4 >> ~/net_monitor/throughput.log
 
-printf "\n ====== END LOOP ======"
+printf "\n ====== END LOOP ======" >> ~/net_monitor/throughput.log
 rm -rf ~/net_monitor/downloads/100kb.txt
 rm -rf ~/net_monitor/downloads/2mb.txt
 rm -rf ~/net_monitor/downloads/100mb.txt
