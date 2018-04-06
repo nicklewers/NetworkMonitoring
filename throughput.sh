a9079f578dff9f0fe681457a8f311741ffa4e6d3
nicklewers <nicklewers@eduroam-19-240.uea.ac.uk>
2018-04-06 13:50:00 +0100

    created script to perform 10 downloads of each file

--- throughput.sh
+++ throughput.sh
@@ -1,10 +1,18 @@
 # download 3 different files sizes 10 times each on a specific time/day
-
+printf "\n ====== BEGIN LOOP ====== "
 # small file
+printf "\n small  file"
 wget -O ~/net_monitor/downloads/100kb.txt https://mirror.aarnet.edu.au/pub/ftp.kernel.org/linux/docs/lanana/device-list/devices-2.6.txt
 
+printf "\n medium file"
 # medium file
 wget -O ~/net_monitor/downloads/2mb.zip https://mirror.aarnet.edu.au/pub/ftp.kernel.org/linux/docs/man-pages/man-pages-3.80.tar.gz
 
+printf "\n large file"
 # large file
 wget -O ~/net_monitor/downloads/100mb.zip https://mirror.aarnet.edu.au/pub/TED-talks/RobinChase_2012G-480p.mp4
+
+printf "\n ====== END LOOP ======"
+rm -rf ~/net_monitor/downloads/100kb.txt
+rm -rf ~/net_monitor/downloads/2mb.txt
+rm -rf ~/net_monitor/downloads/100mb.txt
